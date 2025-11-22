38020f66989bcbd5fd794c0443db51d47f009279
Ulith <ulith.software@gmail.com>
2025-11-22 14:59:17 -0500

    Update inventory.py

--- HU/inventory.py
+++ HU/inventory.py
@@ -1,16 +1,13 @@
-import csv
 
-with open("inventory.csv", "r") as inventory:
-    reader = csv.DictReader(inventory)
-    for position in reader:
-        print(position)
-
-
-
-with open("nuevo.csv", "w", newline="") as f:
-    campos = ["nombre", "edad"]
-    escritor = csv.DictWriter(f, fieldnames=campos)
-
-    escritor.writeheader()  
-
-    escritor.writerow({"nombre": "Ulith", "edad": 20})
+inventory = [
+    {"name" : "milk", "price" : 3000, "quantity" : 20},
+    {"name": "bread", "price": 1000, "quantity": 35},
+    {"name": "eggs", "price": 500, "quantity": 50},
+    {"name": "cheese", "price": 4000, "quantity": 10},
+    {"name": "butter", "price": 5000, "quantity": 8},
+    {"name": "apples", "price": 1000, "quantity": 25},
+    {"name": "oranges", "price": 900, "quantity": 30},
+    {"name": "Jumbo-bar", "price": 4500, "quantity": 15},
+    {"name": "yogurt-alpina", "price": 3500, "quantity": 12},
+    {"name": "coffee-bag", "price": 9000, "quantity": 15}
+    ]
