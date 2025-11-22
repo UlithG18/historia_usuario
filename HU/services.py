0a394cba813e38c235880940d548cdc488f8cc26
Ulith <ulith.software@gmail.com>
2025-11-22 14:58:56 -0500

    Update services.py

--- HU/services.py
+++ HU/services.py
@@ -1,3 +1,209 @@
+import os
+import time
+
+def restart():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def validate_name(menu, message):
+    while True:  
+        print(menu)
+        name = input(message).strip().lower()
+
+        if not name:
+            print("You need to enter a product")
+            continue
+
+        if not all(letters.isalnum() or letters.isspace() or letters == "-" for letters in name):
+            print("The product cannot contain special characters but hyphens")
+            continue
+        return name
+
+def validate_integer(menu, message, min=None, max=None):
+    while True:
+        print(menu)
+        try:
+            value = int(input(message).strip())
+            if min is not None and value < min or max is not None and value > max:
+                raise ValueError
+            return value
+        except ValueError:
+            print("Invalid option, enter a valid menu option\n")
+
+def add_product(inventory, name=None, price=None, quantity=None):
+    data = False
+    while not data:
+        
+        if not name:    
+            name = validate_name("", "\nWhich product do you want to register?: ")
+
+        if price is None:
+            try:
+                price = float(input("\nWhich price do the product have?: "))
+                
+                if not price:
+                    print("\nThe product's price cannot be empty")
+                    continue
+  
+            except ValueError:
+                print("\nYou need to entener a valid value")
+                price = 0
+                continue
+
+        if price <= 0:
+            print("\nThe product's price must be greater than 0")
+            price = 0
+            continue
+            
+        if quantity is None:          
+            quantity = validate_integer("","\nHow many products are you going to register?: ")
+
+        data = True
+
+    inventory.append({"name": name, "price": price, "quantity": quantity})
+    print(f"Product {name} added successfully")
+    time.sleep(4)
+
+    
+
+
+def show_inventory(inventory):
+    if not inventory:
+        print("Inventory is empty.")
+        time.sleep(2)
+        return
+
+    print("\n--- Inventory ---\n")
+    
+    for product in inventory:
+        print(f"Name: {product['name']}")
+        print(f"Price: ${product['price']}")
+        print(f"Quantity: {product['quantity']}")
+    
+    time.sleep(10)
+    return
+
+
+def look_for_product(inventory, name=None):
+    while True:
+        if not name:    
+            name = validate_name("", "Which is the product you are looking for: ")
+
+        found = False
+        for product in inventory:
+            
+            if product["name"] == name:
+                print("Product found successfully: \n", product)
+                time.sleep(5)
+                found = True
+                return
+        if not found:
+            print("There isn't any product with that name")
+            time.sleep(4)
+
+
+def update_product(inventory, name=None, new_price=None, new_quantity=None):
+    while True:
+        if not name:    
+            name = validate_name("", "Which is the product you want to update: ")
+        
+        break
+    
+    found = False
+    for product in inventory:
+        
+        if product["name"] == name:
+            found = True
+        
+            print("\n--- Update menu ---\n1. Name\n2. Price\n3. Quantity\n4. Exit")
+            option = validate_integer("", "What information do you want to change?: ", 1, 4)
+        
+            match option:
+            
+                case 1:
+                    while True:
+                        new_name = input("How do you want to rename this product").strip().lower()
+
+                        if not new_name:
+                            print("The product's name cannot be empty")
+                            continue
+
+                        if not all(ch.isalnum() or ch.isspace() or ch == "-" for ch in new_name):
+                            print("The product only contains letters, spaces or hyphens")
+                            continue
+                    
+                        
+                        product["name"] = new_name
+                        print("Product name changed successfully")
+                        time.sleep(4)
+                        return
+            
+                case 2:
+                    while True:     
+                        try:
+                            new_price = float(input("What is the new price for this product?: "))
+                            break
+
+                        except ValueError:
+                            print("Enter a valid value")
+                                                        
+                    product["price"] = new_price
+                    print("Product price changed successfully")
+                    time.sleep(4)
+                    return     
+            
+                case 3:
+                    new_quantity = validate_integer("", "What is the new quantity for this product?: ")  
+                    product["quantity"] = new_quantity 
+                    print("Product quantity changed successfully")
+                    time.sleep(4)
+                    return   
+            
+                case 4:
+                    print("Exiting...")
+                    time.sleep(2)
+                    return
+    if not found:
+        print("There isn't any product with that name") 
+        time.sleep(2)
+    return     
+    
+
+def product_delete(inventory, name=None):
+
+    while True:
+        if not name:    
+            name = validate_name("", "Which is the product you want to delete: ")
+            break
+    
+    found = False
+    
+    for product in inventory:
+        if product["name"] == name:
+            found = True
+            inventory.remove(product)
+            print(f"The product {name} has been remove")
+            time.sleep(4)
+            return
+    
+    if not found:
+        print("There isn't any product with that name")
+        time.sleep(2)
+
+def calculate_statistics(inventory):
+    if inventory:
+        print("\n--- Statistics menu ---\n")
+
+        total_unities = 0
+        for product in inventory:
+            total_unities += product["quantity"]
+
+        print(f"Total units in inventory: {total_unities}")
+        
+        total_value = 0
+        total_value = sum(product["price"] * product["quantity"] for product in inventory)
+        
+        print(f"Total value of inventory: ${total_value}")
+
 # agregar_producto(inventario, nombre, precio, cantidad)
 # mostrar_inventario(inventario)
 # buscar_producto(inventario, nombre) → retorna el dict o None
@@ -65,129 +271,30 @@ def add_product(inventory, name, price, quantity):
         if not quantity:          
             quantity = validate_integer("How many products are you going to register?: ")
 
-        data = True
 
-    inventory.append({"name": name, "price": price, "quantity": quantity})
-    print(f"Product {name} added successfully")
-
-    
-
-
-def show_inventory(inventory):
-        print(inventory)
-
-
-def look_for_product(inventory, name):
-    while True:
-        if not name:    
-            name = input("Which is the product you are looking for: ").strip().lower()
-
-            if not name:
-                print("You need to enter a product")
-                continue
+        exp_product = inventory[0] 
+        for product in inventory[1:]:     
+            if product["price"] > exp_product["price"]:
+                exp_product = product  
         
-            if not all(letters.isalnum() or letters.isspace() or letters == "-" for letters in name):
-                print("The product cannot contain special characters but hyphens")
-                continue
-
-            found = False
-            for product in inventory:
-                
-                if product["name"] == name:
-                    print(product)
-                    found = True
-                    return
-            if not found:
-                print("There isn't any product with that name") 
-
-look_for_product(inventory, name)
-                    
-
-
-
-def update_product(inventory, name, new_price=None, new_quantity=None):
-    while True:
-        name = input("Which is the product you want to update: ")
+        print(f"The most expensive product: {exp_product['name']} | ${exp_product['price']}")
         
-        if not name:
-            print("You need to enter a product")
-            continue
-    
-        if not all(ch.isalnum() or ch.isspace() or ch == "-" for ch in name):
-            print("The product cannot contain special characters but hyphens")
-            continue
-
-        break
-    found = False
-    for product in range(inventory):
         
-        if product["name"] == name:
-            found = True
-            while True:
-                print("\n--- Update menu ---\n1. Name\n2. Price\n3. Quantity\n4. Exit")
-                option = validate_integer("What information do you want to change?: ", 1, 4)
-            
-                match option:
-                
-                    case 1:
-                        while True:
-                            new_name = input("How do you want to rename this product").strip().lower()
+        high_stock = inventory[0]
+        for product in inventory[1:]:
+            if product["quantity"] > high_stock["quantity"]:
+                high_stock = product
 
-                            if not new_name:
-                                print("The product's name cannot be empty")
-                                continue
+        print(f"The highest stock product: {high_stock['name']} | {high_stock['quantity']}")
 
-                            if not all(ch.isalnum() or ch.isspace() or ch == "-" for ch in new_name):
-                                print("The product only contains letters, spaces or hyphens")
-                                continue
-                        
-                           
-                            product["name"] = new_name
-                            return
-                
-                    case 2:
-                        while True:    
-                            
-                            try:
-                                new_price = float(input("What is the new price for this product?: "))
-                                break
-
-                            except ValueError:
-                                print("Enter a valid value")
-                                                            
-                        product["price"] = new_price
-                        return     
-                
-                    case 3:
-                        new_quantity = validate_integer("What is the new quantity for this product?: ")  
-                        product["quantity"] = new_quantity 
-                        return   
-                
-                    case 4:
-                        return
-    if not found:
-        print("There isn't any product with that name") 
-    return     
-
-
-def product_delete(inventory, name):
-
-    while True:
-        if not name:    
-            name = validate_name("Which is the product you want to delete: ")
-            break
+        sub_total = lambda product: product["price"] * product["quantity"]
+        for product in inventory:
+            print(f"The subtotal of the product: {product['name']} | ${sub_total(product)}")
+        time.sleep(10)
+        return
     
-    found = False
-    
-    for product in inventory:
-        if product["name"] == name:
-            found = True
-            inventory.remove(product)
-            print(f"The product {name} has been remove")
-            return
-    
-    if not found:
-        print("There isn't any product with that name")
-
-# def calcular_estadisticas(inventario):
+    if not inventory:
+        print("Inventory is empty.")
+        time.sleep(4)
+    return
 
